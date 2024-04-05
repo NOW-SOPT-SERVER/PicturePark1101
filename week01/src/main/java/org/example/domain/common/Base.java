773bec0730053238abcbc994a9b7ea8db5b95c9d
Parkseojin <seojin5565@gmail.com>
2024-04-06 03:21:15 +0900

    [feat] : Base 생성자, 포맷 출력 함수 추가 #1

--- week01/src/main/java/org/example/domain/common/Base.java
+++ week01/src/main/java/org/example/domain/common/Base.java
@@ -1,6 +1,7 @@
 package org.example.domain.common;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import lombok.Getter;
 
 @Getter
@@ -10,4 +11,13 @@ public abstract class Base {
 
   private LocalDateTime updatedAt;
 
+  public Base() {
+    this.createdAt = LocalDateTime.now();
+    this.updatedAt = LocalDateTime.now();
+  }
+
+  public String getFormattedDate() {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    return this.createdAt.format(formatter);
+  }
 }
