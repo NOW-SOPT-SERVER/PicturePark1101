6ad5fccd41c5a7d3ddcb778f2afc8aa0895fd158
Parkseojin <seojin5565@gmail.com>
2024-04-06 03:22:12 +0900

    [feat] : Person에 정적 메소드 팩토리 추가 #1

--- week01/src/main/java/org/example/domain/Person.java
+++ week01/src/main/java/org/example/domain/Person.java
@@ -7,15 +7,26 @@ import org.example.domain.enums.Gender;
 
 @Getter
 @Setter
-public abstract class Person extends Base {
+public class Person extends Base {
 
+  private Base base;
   private String name;
   private String socialId;
-
   private Gender gender;
-
   private String address;
-
   private int age;
+
+  private Person(String name, String socialId, Gender gender, String address, int age) {
+    super();
+    this.name = name;
+    this.socialId = socialId;
+    this.gender = gender;
+    this.address = address;
+    this.age = age;
+  }
+
+  public static Person of(String name, String socialId, Gender gender, String address, int age) {
+    return new Person(name, socialId, gender, address, age);
+  }
 
 }
