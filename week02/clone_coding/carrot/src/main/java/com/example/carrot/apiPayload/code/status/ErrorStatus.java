3d6c02121f117c93acce7b8dd2cff46d7f67f199
Parkseojin <seojin5565@gmail.com>
2024-04-17 12:11:41 +0900

    add : 글작성 관련 에러상황메시지, 성공메시지 추가  #4

--- week02/clone_coding/carrot/src/main/java/com/example/carrot/apiPayload/code/status/ErrorStatus.java
+++ week02/clone_coding/carrot/src/main/java/com/example/carrot/apiPayload/code/status/ErrorStatus.java
@@ -9,12 +9,20 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorStatus implements BaseErrorCode {
-    // 가장 일반적인 응답
+
     _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
     _BAD_REQUEST(HttpStatus.BAD_REQUEST,"COMMON400","잘못된 요청입니다."),
     _UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"COMMON401","인증이 필요합니다."),
-    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다.");
-    // For test
+    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
+
+    // 판매글 관련(Product)
+    _PRODUCT_BAD_REQUEST_TRANSACTION_METHOD(HttpStatus.BAD_REQUEST,"PRODUCT400","해당 거래 방식은 존재하지 않습니다. 다시 확인부탁드립니다."),
+    _PRODUCT_BAD_REQUEST_CATEGROY(HttpStatus.BAD_REQUEST,"PRODUCT400","해당 카테고리 ID는 존재하지 않습니다. 다시 확인부탁드립니다."),
+
+    // member..
+    _MEMBER_NOT_FIND(HttpStatus.UNAUTHORIZED,"MEMBER401","해당 회원은 존재하지 않습니다."),
+    _CATEGORY_NOT_FIND(HttpStatus.BAD_REQUEST,"CATEGORY400","해당 카테고리 ID는 존재하지 않습니다.");
+
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
--- week02/clone_coding/carrot/src/main/java/com/example/carrot/apiPayload/code/status/SuccessStatus.java
+++ week02/clone_coding/carrot/src/main/java/com/example/carrot/apiPayload/code/status/SuccessStatus.java
@@ -13,20 +13,8 @@ public enum SuccessStatus implements BaseCode {
     // 일반적인 응답
     _OK(HttpStatus.OK, "COMMON200", "성공입니다."),
 
-    // 멤버 관련 응답
+    _PRODUCT_BAD_REQUEST_TRANSACTION_METHOD(HttpStatus.OK, "PRODUCT200", "판매글 생성에 성공하였습니다.");
 
-    // 임장 관련 응답
-    LIMJANG_DELETE(HttpStatus.OK, "LIMJANG2000", "임장 게시글 삭제 성공하였습니다."),
-    LIMJANG_UPDATE(HttpStatus.OK, "LIMJANG2001", "임장 게시글 수정 성공하였습니다."),
-
-
-    // 스크랩 관련 응답
-    _SCRAP_ACTION_SCRAP(HttpStatus.OK, "SCRAP2000", "스크랩 추가 성공하였습니다."),
-    _SCRAP_ACTION_UNSCRAP(HttpStatus.OK, "SCRAP2001", "스크랩 취소 성공하였습니다."),
-
-    // 이미지 관련 응답
-    IMAGE_UPDATE(HttpStatus.OK, "IMAGE2000", "이미지 업로드 성공하였습니다."),
-    IMAGE_DELETE(HttpStatus.OK, "IMAGE2001", "이미지 삭제 성공하였습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
