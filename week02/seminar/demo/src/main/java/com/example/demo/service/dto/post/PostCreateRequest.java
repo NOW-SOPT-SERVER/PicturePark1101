3577cc3dce7bc2ba259e8e037318de5ff9e9f424
Parkseojin <seojin5565@gmail.com>
2024-05-01 17:23:40 +0900

    add : 3차 세미나 과제_PostCreateRequest record 생성  #7

---
+++ week02/seminar/demo/src/main/java/com/example/demo/service/dto/post/PostCreateRequest.java
@@ -0,0 +1,14 @@
+package com.example.demo.service.dto.post;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record PostCreateRequest(
+    @NotBlank(message = "글의 제목은 필수입니다.")
+    String name,
+
+    @NotBlank(message = "글의 내용은 필수입니다.")
+    String content
+
+){}
+// name과 content가 필요함
