21c3f51895bd5cc883415005c8aafeea8b1f29ae
Parkseojin <seojin5565@gmail.com>
2024-05-05 21:37:07 +0900

    feature : message 추가 #9

--- week02/clone_coding/carrot/src/main/java/com/example/carrot/apiPayload/dto/SuccessMessage.java
+++ week02/clone_coding/carrot/src/main/java/com/example/carrot/apiPayload/dto/SuccessMessage.java
@@ -9,7 +9,10 @@ import org.springframework.http.HttpStatus;
 public enum SuccessMessage {
 
   PRODUCT_POST_CREATE_SUCCESS(HttpStatus.CREATED.value(), "포스트 생성이 완료되었습니다."),
-  PRODUCT_POST_FIND_SUCCESS(HttpStatus.OK.value(), "지역별 조회 완료되었습니다.");
+  PRODUCT_POST_FIND_SUCCESS(HttpStatus.OK.value(), "지역별 조회 완료되었습니다."),
+  PRODUCTLIKE_SUCCESS_ADDED(HttpStatus.CREATED.value(), "좋아요 추가 완료되었습니다."),
+  PRODUCTLIKE_SUCCESS_REMOVED(HttpStatus.OK.value(), "좋아요 삭제 완료되었습니다.")
+  ;
 
   private final int status;
   private final String message;
