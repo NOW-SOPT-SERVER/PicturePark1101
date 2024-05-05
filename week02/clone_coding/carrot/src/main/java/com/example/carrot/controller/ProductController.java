3876b33f984e51bbd495951cadc45982ca8e8039
Parkseojin <seojin5565@gmail.com>
2024-05-05 17:07:35 +0900

    feature : 지역별 글조회 controller #9

--- week02/clone_coding/carrot/src/main/java/com/example/carrot/controller/ProductController.java
+++ week02/clone_coding/carrot/src/main/java/com/example/carrot/controller/ProductController.java
@@ -6,16 +6,22 @@ import com.example.carrot.apiPayload.dto.SuccessStatusResponse;
 import com.example.carrot.model.dto.request.product.ProductPostRequestDTO;
 import com.example.carrot.model.dto.response.product.ProductPostResposeDTO;
 import com.example.carrot.service.product.ProductCommandService;
+import com.example.carrot.service.product.ProductQueryService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import java.net.URI;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,10 +29,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Validated
 public class ProductController {
+
   private final ProductCommandService productCommandService;
+  private final ProductQueryService productQueryService;
 
   @Operation(summary = "글 생성 API입니다.")
-  @PostMapping("")
+  @PostMapping
   public ResponseEntity<SuccessStatusResponse> postLimjang(
       @RequestBody @Valid ProductPostRequestDTO postDTO
   ){
@@ -36,6 +44,13 @@ public class ProductController {
         .body(SuccessStatusResponse.of(SuccessMessage.POST_CREATE_SUCCESS));
   }
 
+  @Operation(summary = "지역별 상품 LIST API입니다.")
+  @GetMapping
+  public ResponseEntity<SuccessStatusResponse> getPostsByRegion  (
+      @RequestParam @NotBlank String region
+  ){
 
+    return ResponseEntity.ok(productQueryService.findByRegion(region));
+  }
 
 }
