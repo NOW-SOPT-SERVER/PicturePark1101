185fcf3b4fd488ec4f0929f6fccaa2406c09c9b6
Parkseojin <seojin5565@gmail.com>
2024-05-05 16:28:31 +0900

    refactor : apiresponse 관련 수정 #9

--- week02/clone_coding/carrot/src/main/java/com/example/carrot/apiPayload/dto/SuccessMessage.java
+++ week02/clone_coding/carrot/src/main/java/com/example/carrot/apiPayload/dto/SuccessMessage.java
@@ -8,10 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum SuccessMessage {
 
-  BLOG_CREATE_SUCCESS(HttpStatus.CREATED.value(), "블로그 생성이 완료되었습니다."),
-  POST_CREATE_SUCCESS(HttpStatus.CREATED.value(), "블로그 포스트 생성이 완료되었습니다."),
-  POST_FIND_SUCCESS(HttpStatus.OK.value(), "블로그 포스트 조회에 성공했습니다.");
-
+  POST_CREATE_SUCCESS(HttpStatus.CREATED.value(), "포스트 생성이 완료되었습니다.");
 
   private final int status;
   private final String message;
