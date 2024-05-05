2de287377ab94b2a7c38681cd71276793e13da6a
Parkseojin <seojin5565@gmail.com>
2024-05-05 18:22:24 +0900

    feature : 상품좋아요, 이미지 ErrorMessage 추가 #9

--- week02/clone_coding/carrot/src/main/java/com/example/carrot/apiPayload/dto/ErrorMessage.java
+++ week02/clone_coding/carrot/src/main/java/com/example/carrot/apiPayload/dto/ErrorMessage.java
@@ -10,7 +10,10 @@ public enum ErrorMessage {
 
   MEMBER_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 사용자가 존재하지 않습니다."),
   CATEGORY_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 카테고리가 존재하지 않습니다."),
-  TRANSACTION_METHOD_NOT_VALIDATED(HttpStatus.NOT_FOUND.value(), "해당 거래방식은 지정된 방식이 아닙니다.");
+  TRANSACTION_METHOD_NOT_VALIDATED(HttpStatus.NOT_FOUND.value(), "해당 거래방식은 지정된 방식이 아닙니다."),
+  PRODUCT_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 상품이 존재하지 않습니다."),
+  PRODUCTIMAGE_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 상품 사진이 존재하지 않습니다."),
+  PRODUCTLIKE_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 좋아요 글이 존재하지 않습니다.");
   ;
 
   private final int status;
