1fc6bde5a3475f36481affbbd35aecc31ba5e5a9
Parkseojin <seojin5565@gmail.com>
2024-05-05 21:03:22 +0900

    feature : product id로 찾는 로직 추가 #9

--- week02/clone_coding/carrot/src/main/java/com/example/carrot/service/product/ProductQueryServiceImpl.java
+++ week02/clone_coding/carrot/src/main/java/com/example/carrot/service/product/ProductQueryServiceImpl.java
@@ -1,12 +1,16 @@
 package com.example.carrot.service.product;
 
 
+import com.example.carrot.apiPayload.dto.ErrorMessage;
+import com.example.carrot.exception.NotFoundException;
 import com.example.carrot.model.dto.response.product.ProductFindResponseDto;
 import com.example.carrot.model.dto.response.product.ProductListFindResponseDto;
+import com.example.carrot.model.entity.Product;
 import com.example.carrot.repository.ChatroomRepository;
 import com.example.carrot.repository.ProductRepository;
+import com.example.carrot.service.chatroom.ChatroomService;
 import com.example.carrot.service.productimage.ProductImageService;
-import com.example.carrot.service.productlike.ProductLikeService;
+import com.example.carrot.service.productlike.ProductLikeCommandService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,8 +21,9 @@ public class ProductQueryServiceImpl implements ProductQueryService{
 
   private final ProductRepository productRepository;
   private final ProductImageService productImageService;
-  private final ProductLikeService productLikeService;
-  private final ChatroomRepository chatroomRepository;
+  private final ProductLikeCommandService productLikeCommandService;
+  private final ChatroomService chatroomService;
+
 
   public ProductListFindResponseDto findByRegion(Long regionId){
 
@@ -26,13 +31,19 @@ public class ProductQueryServiceImpl implements ProductQueryService{
         .stream()
         .map(product -> {
           String productImage = productImageService.findByProductId(product.getId());
-          Long cntLike = productLikeService.countByProductId(product.getId());
-          Long cntChatroom = chatroomRepository.countByProductId(product.getId());
+          Long cntLike = productLikeCommandService.countByProductId(product.getId());
+          Long cntChatroom = chatroomService.countByProductId(product.getId());
 
           return ProductFindResponseDto.of(product, productImage, cntLike, cntChatroom);
         }).toList();
 
     return new ProductListFindResponseDto(productFindResponseDtoList);
+  }
+
+  public Product findById(Long id) {
+    return productRepository.findById(id)
+        .orElseThrow(
+            () -> new NotFoundException(ErrorMessage.PRODUCT_NOT_FOUND_BY_ID_EXCEPTION));
   }
 
 }
