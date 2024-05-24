339f1ebb36d98cc5c688e9dba6e5f6164fc53e74
Parkseojin <seojin5565@gmail.com>
2024-05-25 04:13:25 +0900

    setting : 4차 세미나

--- week02/seminar/demo/src/main/java/com/example/demo/service/dto/post/PostCreateRequest.java
+++ week02/seminar/demo/src/main/java/com/example/demo/service/dto/post/PostCreateRequest.java
@@ -1,5 +1,7 @@
 package com.example.demo.service.dto.post;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record PostCreateRequest(
     @NotBlank(message = "글의 제목은 필수입니다.")
     String name,
