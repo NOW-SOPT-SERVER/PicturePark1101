ec15cefc09cade0f619344c82671255c14569be5
Parkseojin <seojin5565@gmail.com>
2024-05-30 16:30:54 +0900

    feat : dto에 refresh 토큰 필드 추가  #15

--- week02/seminar/demo/src/main/java/com/example/demo/service/dto/member/UserJoinResponse.java
+++ week02/seminar/demo/src/main/java/com/example/demo/service/dto/member/UserJoinResponse.java
@@ -1,14 +1,16 @@
-package com.example.demo.service.dto;
+package com.example.demo.service.dto.member;
 
 public record UserJoinResponse(
     String accessToken,
+    String refreshToekn,
     String userId
 ) {
 
   public static UserJoinResponse of(
       String accessToken,
+      String refreshToekn,
       String userId
   ) {
-    return new UserJoinResponse(accessToken, userId);
+    return new UserJoinResponse(accessToken, refreshToekn, userId);
   }
 }
